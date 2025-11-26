14d77d3b1b2fa4e02f88f0ad4726ed69ce65d754
cirorey20 <cirorey95@gmail.com>
2025-11-26 00:33:17 -0300

    agregue archivo para ejemplificar las variables de entorno

---
+++ config/env.example.php
@@ -0,0 +1,15 @@
+<?php
+//ESTE ARCHIVO ES DE EJEMPLO, NO DEBE TOCARSE
+/*
+INSTRUCCIONES:
+Lo que se debe hacer es crear un archivo env.php dentro de
+la carpeta config/ y ahi copiar estas lineas de codigo.
+
+Luego dentro del archivo env.php si poner las credenciales de tu: 
+localhost, base de datos, usuario y contraseña.
+*/
+
+putenv("DB_HOST=nombre-del-servidor");
+putenv("DB_NAME=nombre-de-la-base-de-datos");
+putenv("DB_USER=nombre-de-usuario");
+putenv("DB_PASS=contraseña");
