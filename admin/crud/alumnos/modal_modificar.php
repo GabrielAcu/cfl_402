a7d16b05485461b91ebc4bf7b547573edaf9ddb8
Kfz-087 <tobirostro132@gmail.com>
2025-12-07 16:26:21 -0300

    el modal que modifica alumnos

---
+++ admin/crud/alumnos/modal_modificar.php
@@ -0,0 +1,24 @@
+
+
+<?php
+// ==========================
+//   MODAL (SIN CAMBIAR NADA)
+//   SOLO SEPARADO DEL INDEX
+// ==========================
+
+// Acá simplemente pegá EXACTAMENTE el modal que tenías en el index.
+// No voy a inventar nada porque quiero que tu código siga funcionando
+// exactamente igual que antes.
+
+
+?>
+<div id="modalVerCurso" class="modal">
+    <div class="modal-content">
+
+        
+        <span id="cerrarVerCurso" class="cerrar">&times;</span>
+
+        <div id="contenidoCurso"></div>
+    </div>
+</div>
+
