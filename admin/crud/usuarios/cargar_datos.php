a75205cbcb57ac81131f5bd5ecb68bac8912c4be
Kfz-087 <tobirostro132@gmail.com>
2025-12-08 21:36:51 -0300

    solución de error

--- admin/crud/usuarios/cargar_datos.php
+++ admin/crud/usuarios/cargar_datos.php
@@ -2,6 +2,20 @@
     require_once dirname(__DIR__, 3) . '/config/path.php';
 require_once BASE_PATH . '/config/conexion.php';
 
+// ==========================
+//   CONFIGURACIÓN INICIAL
+// ==========================
+
+require_once BASE_PATH . '/auth/check.php';
+// require_once BASE_PATH . '/include/header.php';
+// require_once 'layouts.php';
+
+if (!isSuperAdmin()) {
+    header('Location: /cfl_402/index.php');
+    exit();
+}
+
+
 $conn = conectar();
 
 // Validar GET
