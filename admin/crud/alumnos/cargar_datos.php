8f001724ec47a48c6278fe2371313f6813a023e8
Kfz-087 <tobirostro132@gmail.com>
2025-12-09 22:26:57 -0300

    pruebas de código, todo sigue funcional

--- admin/crud/alumnos/cargar_datos.php
+++ admin/crud/alumnos/cargar_datos.php
@@ -14,8 +14,8 @@ $id = $_GET["id_alumno"];
 
 // ========== OBTENER DATOS DEL ALUMNO ==============
 $sql = $conn->prepare("
-    SELECT *
-    FROM alumnos
+      SELECT *
+     FROM alumnos
     WHERE id_alumno = :id
 ");
 $sql->execute([":id" => $id]);
@@ -26,7 +26,41 @@ if (!$alumno) {
     exit;
 }
 
+    // SELECT *
+    // FROM alumnos
+    // WHERE id_alumno = :id
+
+// SELECT alumnos.*,
+//     CASE alumnos.nombre WHEN null THEN 'No Ingresado'
+//     CASE alumnos.apellido WHEN null THEN 'No Ingresado'
+//     CASE alumnos.dni WHEN null THEN 'No Ingresado'
+//     CASE alumnos.fecha_nacimiento WHEN null THEN 'No Ingresado'
+//     CASE alumnos.telefono WHEN null THEN 'No Ingresado'
+//     CASE alumnos.correo WHEN null THEN 'No Ingresado'
+//     CASE alumnos.direccion WHEN null THEN 'No Ingresado'
+//     CASE alumnos.localidad WHEN null THEN 'No Ingresado'
+//     CASE alumnos.cp WHEN null THEN 'No Ingresado'
+//     CASE alumnos.vehiculo WHEN null THEN 'No Ingresado'
+//     CASE alumnos.patente WHEN null THEN 'No Ingresado'
+//     CASE alumnos.observaciones WHEN null THEN 'No Ingresado'
+//     ELSE 'No existe'
+//     END AS Datos
+//     FROM alumnos
+//     WHERE id_alumno = :id
+
+
 // ========== RESPUESTA JSON ==============
 echo json_encode([
     "alumno" => $alumno
 ]);
+//      alumnos.apellido,
+//      alumnos.dni ,
+//      alumnos.fecha_nacimiento ,
+//      alumnos.telefono ,
+//      alumnos.correo ,
+//      alumnos.direccion ,
+//      alumnos.localidad,
+//      alumnos.cp,
+//      alumnos.vehiculo,
+//     alumnos.patente,
+//      alumnos.observaciones
