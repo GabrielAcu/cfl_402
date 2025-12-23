69df644964992a9ededa63d6fd4106cc8886630e
Kfz-087 <tobirostro132@gmail.com>
2025-12-22 23:38:54 -0300

    modifiqué el form para ponerle css

--- admin/crud/contacto/form_contacto.php
+++ admin/crud/contacto/form_contacto.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agregar Contactos</title>
+    <link rel="stylesheet" href="contactos2.css">
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 session_start();
 // 3. Autenticación
@@ -25,26 +37,99 @@ if ($tipo == null || $id_entidad == null) {
 }
 
 
-echo "Agregar contacto<br><br>";
+// echo "Agregar contacto<br><br>";
 
 require_once BASE_PATH . '/config/csrf.php';
 
 echo "
-<form action='procesar_contacto.php' method='post'>
+<form action='procesar_contacto.php' method='post' id='modalContacto' class='form-contacto'>
     " . getCSRFTokenField() . "
-    <input type='text' name='nombre' placeholder='Nombre' required>
-    <input type='text' name='apellido' placeholder='Apellido' required>
-    <input type='number' name='dni' placeholder='DNI' required>
-    <input type='number' name='telefono' placeholder='Telefeno' required>
-    <input type='email' name='correo' placeholder='Correo' required>
-    <input type='adress' name='direccion' placeholder='Dirección' required>
-    <input type='text' name='localidad' placeholder='Localidad' required>
-    <input type='text' name='cp' placeholder='Código Postal' required>
-    <input type='text' name='parentesco' placeholder='Parentesco'required>
-    <textarea name='observaciones' placeholder='Observaciones'></textarea>
-    <input type='number' name='id_entidad' value='{$id_entidad}' readonly>
-    <input type='text' name='tipo' value='{$tipo}' readonly>
-    <input type='submit' value='Enviar'>
+
+
+    <div class='form-wrapper'>
+        <div class='form-scroll'>
+
+            <h3> Nuevo Contacto </h3>  
+
+            <div class='fila'>
+                <div class='campo'>
+                    <label for='nombre-contacto'> Nombre de contacto: </label>
+                    <input class='input-modify' id='nombre-contacto' type='text' name='nombre' placeholder='Nombre' required>
+                </div>
+            
+                <div class='campo'>
+                    <label for='apellido-contacto' > Apellido de contacto: </label>
+                    <input class='input-modify' id='apellido-contacto' type='text' name='apellido' placeholder='Apellido' required>
+                </div
+            </div>
+
+            <div class='fila'>
+                <div class='campo'>
+                    <label for='dni-contacto'> DNI de contacto: </label>
+                    <input class='input-modify' id='dni-contacto' type='number' name='dni' placeholder='DNI' required>
+                </div>
+                <div class='campo'>
+                    <label for='telefono-contacto'> Teléfono de contacto: </label>
+                    <input class='input-modify' id='telefono-contacto' type='number' name='telefono' placeholder='Telefeno' required>
+                </div>
+            </div>
+
+            <div class='fila'>
+                <div class='campo'>
+                    <label for='correo-contacto'> Correo de contacto: </label>
+                    <input class='input-modify' id='correo-contacto' type='email' name='correo' placeholder='Correo' required>
+                </div>
+                <div class='campo'>
+                    <label for='direccion-contacto'> Dirección de contacto: </label>
+                    <input class='input-modify' id='direccion-contacto' type='adress' name='direccion' placeholder='Dirección' required>
+                </div>        
+            </div>
+
+            <div class='fila'>
+                <div class='campo'>
+                    <label for='localidad-contacto'> Localidad de Contacto: </label>
+                    <input class='input-modify' id='localidad-contacto' type='text' name='localidad' placeholder='Localidad' required>
+                </div>
+
+                <div class='campo'>
+                    <label for='cp-contacto'> Código Postal de contacto: </label>
+                    <input class='input-modify' id='cp-contacto' type='text' name='cp' placeholder='Código Postal' required>
+                </div>
+
+            </div>
+
+            <div class='fila'>
+                <div class='campo'>
+                    <label for='parentesco-contacto'> Parentesco de contacto: </label>
+                    <input class='input-modify' 'parentesco-contacto' type='text' name='parentesco' placeholder='Parentesco'required>
+                </div>
+
+                <div class='campo'>
+                    <label for='observaciones-contacto'> Observaciones de Contacto: </label>
+                    <textarea class='input-modify' id='observaciones-contacto' name='observaciones' placeholder='Observaciones'></textarea>
+                </div>    
+                </div>
+
+            <div class='fila'>
+                <div class='campo'>
+                    <label for='entidad-contacto'> Entidad de Contacto: </label>
+                    <input class='input-modify' id='entidad-contacto' type='number' name='id_entidad' value='{$id_entidad}' readonly>
+                </div>
+                <div class='campo'>
+                    <label for='tipo-contacto'>Tipo de Contacto: </label>
+                    <input  class='input-modify' id='tipo-contacto' type='text' name='tipo' value='{$tipo}' readonly>
+                </div>
+            </div>
+
+            <input class='btn-primary' type='submit' value='Enviar'>
+        </div>
+
+        
+
+    </div>
+       
+
+    
 </form>
 ";
 
@@ -57,7 +142,7 @@ echo "
 <form action='listar_contactos.php' method='post'>
     <input type='hidden' name='id_entidad' value='{$id_entidad}' readonly>
     <input type='hidden' name='tipo' value='{$tipo}' readonly>
-    <input type='submit' value='Volver a lista de contactos'>
+    <input class='btnCancel' type='submit' value='Volver a lista de contactos'>
 </form><br><br>
 ";
 echo "<br><br>";
@@ -80,3 +165,8 @@ if (!$tipo || !$id_entidad) {
 
 
 ?>
+
+
+<script src="modal_nuevo.js">
+
+</script>
